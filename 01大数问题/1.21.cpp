cb7ba29aa220307eceae1ff9008f5b6218e8420a
lofiprogram <47585732+lofiprogram@users.noreply.github.com>
2020-12-29 13:08:10 +0800

    Create 1.21.cpp

---
+++ 01大数问题/1.21.cpp
@@ -0,0 +1,51 @@
+/*
+https://blog.csdn.net/ysz171360154/article/details/85006990
+大数加法(不考虑前导0的问题)
+*/
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100],b[100];//通过字符串对大数进行输入并储存
+    int len1,len2,len;
+    while(scanf("%s %s",a,b))
+    {
+        int x[100]={0},y[100]={0},z[105]={0};//将数组元素全部初始化为
+        int i,j=0,k=0;
+        len1=strlen(a);
+        len2=strlen(b);
+        for(i=len1-1;i>=0;i--)//将两个字符串中的字符转化为数字，并倒序储存到数组中，即字符串为123456，则数组为654321
+        {
+            x[j++]=a[i]-'0';
+        }
+        for(i=len2-1;i>=0;i--)
+        {
+            y[k++]=b[i]-'0';
+        }
+        if(len1>len2)
+            len=len1;
+        else
+            len=len2;
+        int m=0;
+        for(i=0;i<len;i++)
+        {
+            z[i]=(x[i]+y[i]+m)%10;//将所得数的个位存到数组z[i]中去
+            if((x[i]+y[i]+m)>=10)
+                m=1;
+            else
+                m=0;
+        }
+        if((x[i-1]+y[i-1]+m)>=10)//判断运算的最大位的和是否>=10
+            z[i]=1;
+        else
+            i=i-1;
+        for(;i>=0;i--)//倒序输出数组
+            printf("%d",z[i]);
+        printf("\n");
+    }
+    return 0;
+}
+
+
